5049d35d014b3b123b15fa3ab944e482204da570
Jari van Dam <jari@jarivandam.nl>
2020-09-24 14:45:53 +0200

    Added port 80 as serverport in startup script

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'Starting app'
 cd '/home/ec2-user/server/target'
-screen -d -m java -jar functional-testing-0.0.1-SNAPSHOT.jar 
+screen -d -m java -jar functional-testing-0.0.1-SNAPSHOT.jar --server.port=80
